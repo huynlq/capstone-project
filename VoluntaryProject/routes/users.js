a962ffc4b52870e919ea07c7349be41167afad62
HUYNLQSE61661\ADMIN <huynlqse61661@fpt.edu.vn>
2017-02-19 21:54:10 +0700

    Update login and a part of user

--- VoluntaryProject/routes/users.js
+++ VoluntaryProject/routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user_list', { title: 'Users Manager' });
+});
+
+/* GET all users. */
+router.get('/all', function(req, res, next) {
+	var db = req.db;
+    var collection = db.get('Users');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
 });
 
 module.exports = router;
