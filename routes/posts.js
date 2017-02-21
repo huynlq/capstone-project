7eae7c627517246ec73ed596e7887d888953f4fb
khuelm <mingkhuele@gmail.com>
2017-02-21 12:05:37 +0700

    no message

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET posts listing. */
 router.get('/', function(req, res, next) {
   res.render('post_list', { title: 'Post Manager' });
 });
 
-/* GET users listing. */
+/* GET posts listing. */
 router.get('/creator/', function(req, res, next) {
   res.render('post_creator', { title: 'Post Creator' });
 });
